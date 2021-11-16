a77fb844663c00bdd50bc3244088e6f3c5da6f13
migo <saka90030@gmail.com>
2021-11-16 20:16:17 +0900

    good!

--- Git_handling/all_checks.py
+++ Git_handling/all_checks.py
@@ -2,10 +2,10 @@
 import os
 def check_reboot():
     """Returns True is the computer has a pending reboot."""
-    return os.path.exist("/run/reboot-required")
+    return os.path.exists("/run/reboot-required")
     #This is a file that's created on our computer when some software requires a reboot.
 
 def main():
     pass
 
-main()
+print(check_reboot())
