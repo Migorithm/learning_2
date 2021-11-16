ab827916a19394d83d1f4697934d58174692a76c
migo <saka90030@gmail.com>
2021-11-16 20:41:43 +0900

    Revert "dgdgdg"
    This reverts commit a4f61c40bf09e44e3856569b4e59f5e469c13f9b.

--- Git_handling/.ipynb_checkpoints/all_checks-checkpoint.py
+++ Git_handling/.ipynb_checkpoints/all_checks-checkpoint.py
@@ -1,19 +1,11 @@
 #!/usr/bin/python3
 import os
-import sys
 def check_reboot():
     """Returns True is the computer has a pending reboot."""
-    return os.path.exists("/run/reboot-required")
+    return os.path.exist("/run/reboot-required")
     #This is a file that's created on our computer when some software requires a reboot.
 
 def main():
-    if check_reboot():
-        print("Pending Reboot.")
-        sys.exit(1)
-    if disk_full():
-        print("Disk Full.")
-        sys.exit()
-    print("Everything OK.")
-    sys.exit(0)
+    pass
 
 main()
